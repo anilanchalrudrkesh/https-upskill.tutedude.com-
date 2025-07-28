a9dfdcd14f2f9cc663818ec4fe33d7e70e87633b
anilanchalrudrkesh <anil.dhaiya@gmail.com>
2025-07-28 14:22:33 +0530

    Add files via upload

---
+++ Assignment 5 task 1.py
@@ -0,0 +1,7 @@
+a=str(input(f'Eenter the students name : ' ))
+Dicsonary={'anil':82,'Sunil':72,'Alice':85,'sangita':90}
+
+if a in Dicsonary:
+    print(Dicsonary[a])
+else:
+    print(f"Student {a} was not Found")
---
+++ Assignment 5 task 2.py
@@ -0,0 +1,5 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+print(f'orignal List : {list}')
+print(f'Extract First five element {list[0:5]}'  )
+list.reverse()
+print(f'Extract Last five element {list[0:5]}'  )
